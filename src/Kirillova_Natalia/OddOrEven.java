a0c5d0a52e54d8e276a02b877f05258fb737e8c9
nkfl2022 <nkfl@yahoo.com>
2023-05-22 13:57:34 -0400

    created third Java task class

---
+++ src/Kirillova_Natalia/OddOrEven.java
@@ -0,0 +1,12 @@
+package Kirillova_Natalia;
+
+public class OddOrEven {
+}
+/*
+Write a method which can identifies given number is even or odd
+
+ex:
+    identify(5) -> "Odd"
+    identify(6) -> "Even"
+
+ */
