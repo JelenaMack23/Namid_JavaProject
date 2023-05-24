c8452cb0bc6633fca112c9dda25ac185d7852e84
Ersin KAYHAN <dekked2438@gmail.com>
2023-05-24 19:45:18 +0300

    My First Message_2

--- src/week_1/Michael.java
+++ src/week_1/Michael.java
@@ -3,5 +3,6 @@ package week_1;
 public class Michael {
     public static void main(String[] args) {
         System.out.println("Hi Friends, I'm with you!!!");
+        System.out.println("We'll talk about GitHub and our Java Tasks.");
     }
 }
