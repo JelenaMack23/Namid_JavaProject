f5375f967ce7777eb7c3dfb649cbd7eaf89aea0b
nkfl2022 <nkfl@yahoo.com>
2023-05-25 11:02:35 -0400

    solution for task

--- src/Kirillova_Natalia/OddOrEven.java
+++ src/Kirillova_Natalia/OddOrEven.java
@@ -1,6 +1,20 @@
 package Kirillova_Natalia;
 
 public class OddOrEven {
+
+    public static boolean isOdd(int num){
+        if (num % 2 != 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static boolean isEven(int num){
+        return !isOdd(num);
+
+    }
+
 }
 /*
 Write a method which can identifies given number is even or odd
