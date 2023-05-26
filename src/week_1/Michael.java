cfc430c7623783f94a30967735077c5ab9ac3b20
Ersin KAYHAN <dekked2438@gmail.com>
2023-05-26 19:38:25 +0300

    Did first task, started second

--- src/week_1/Michael.java
+++ src/week_1/Michael.java
@@ -4,5 +4,23 @@ public class Michael {
     public static void main(String[] args) {
         System.out.println("Hi Friends, I'm with you!!!");
         System.out.println("We'll talk about GitHub and our Java Tasks.");
+        oddOrEven(51);
+
+        divisionWithoutOperator(56,98);
+
+
+    }
+
+
+    public static void oddOrEven(int num){
+
+
+    }
+
+    public static int divisionWithoutOperator(int num1, int num2){
+
+        int result=num1/num2;
+
+        return result;
     }
 }
