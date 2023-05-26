bb6ff145c8608df3f1d74d4ff7b0b404212e8a89
Nina_Cydeo <marko_lapcevic@yahoo.com>
2023-05-26 08:39:44 -0400

    Tasks

---
+++ src/Nina_Mujezinovic/DivideWithoutDivision.java
@@ -0,0 +1,35 @@
+package Nina_Mujezinovic;
+
+
+public class DivideWithoutDivision {
+
+    public static double DivideWithoutDivision(int a, int b) {
+        double res = 0;
+
+        if (a == 0)
+            return 0;
+        if (b == 0)
+            throw new ArithmeticException("Yon can not divide by 0");
+
+
+        while (a >= b) {
+            a = a - b;// a=30 b= 4 then a = 30-4 ; a = 26
+            res++;
+        }
+
+        boolean isNegative = (a < 0 && b > 0 || a > 0 && b < 0);
+
+            if (isNegative)
+                res = -res;
+
+            return res;
+
+    }
+
+    public static void main(String[] args) {
+        int a = -30;
+        int b = 4;
+        System.out.println(DivideWithoutDivision(a,b));
+    }
+
+}
