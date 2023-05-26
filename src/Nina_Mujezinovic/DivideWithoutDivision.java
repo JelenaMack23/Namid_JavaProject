3dca61ff58ccb469b110d59ebc41b7317d82ef04
Nina_Cydeo <marko_lapcevic@yahoo.com>
2023-05-26 14:07:07 -0400

    Day2 is over

--- src/Nina_Mujezinovic/DivideWithoutDivision.java
+++ src/Nina_Mujezinovic/DivideWithoutDivision.java
@@ -3,25 +3,33 @@ package Nina_Mujezinovic;
 
 public class DivideWithoutDivision {
 
-    public static double DivideWithoutDivision(int a, int b) {
-        double res = 0;
+    public static int DivideWithoutDivision(int a, int b) {
+        int res = 0;
 
+        // in cass that we have 0
         if (a == 0)
             return 0;
         if (b == 0)
             throw new ArithmeticException("Yon can not divide by 0");
 
+        // store sign of the result, in case we have negative number
+        int sign = 1;
+        if (a * b < 0){
+            sign = -1;
+        }
+
+        // covert to positive numbers so we can perform while loop
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+
         while (a >= b) {
             a = a - b;// a=30 b= 4 then a = 30-4 ; a = 26
             res++;
         }
+        System.out.println("And the remain is: " + a);
 
-        boolean isNegative = (a < 0 && b > 0 || a > 0 && b < 0);
-
-            if (isNegative)
-                res = -res;
-
-            return res;
+            return sign * res;
     }
 
     public static void main(String[] args) {
