f326e2a09fb6200a413d22d1c62c801e7cc6d6e3
Nina_Cydeo <marko_lapcevic@yahoo.com>
2023-05-31 19:02:30 -0400

    Week 2,task 1 ( swap 2 numbers )

---
+++ src/week2/Nina.java
@@ -0,0 +1,17 @@
+package week2;
+
+public class Nina {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 15;
+
+         a = a + b;  // a + b = 20
+         b = a - b;  // b 20 - 15 = 5
+         a = a - b;  // a  20 - 5 = 15
+
+        System.out.println("A is: " + a );
+        System.out.println("B is: " + b);
+
+    }
+}
