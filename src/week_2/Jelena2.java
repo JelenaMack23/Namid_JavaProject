94cce1e8bd4ff764952877db80c5d47594b6a4bf
JelenaMack23 <mackovic.jelena@yahoo.com>
2023-05-31 12:01:49 -0400

    Code 2

--- src/week_2/Jelena2.java
+++ src/week_2/Jelena2.java
@@ -13,8 +13,8 @@ Swap two variable values without using a third variable
             int b = 20;
 
             a = a + b;  // a is ab
-            b = a - b;  //  a is ab and - b we have only a left, so b = a
-            a = a - b;  //  a is ab and - new b which is a now we have only b left, so a = b
+            b = a - b;  //  'a' is ab and - b we have only a left, so b = a
+            a = a - b;  //  'a' is ab and - new b which is a now we have only b left, so a = b
 
 
             System.out.println("a is = " + a);
@@ -45,7 +45,6 @@ Swap two variable values without using a third variable
 
                 }
             }
-
         }
 
 
