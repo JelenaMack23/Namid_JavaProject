3c9815289a229e6a2fb96c5973e3f723041c4db7
Nina_Cydeo <marko_lapcevic@yahoo.com>
2023-05-31 20:53:07 -0400

    Week 2,task 2 ( Number division )

--- src/week2/Nina.java
+++ src/week2/Nina.java
@@ -13,5 +13,33 @@ public class Nina {
         System.out.println("A is: " + a );
         System.out.println("B is: " + b);
 
+
+        System.out.println("---------------------------");
+
+        // Task numbers--divisible by 3,5,15
+
+
+        String divisibleBy_3_5_15 = "";
+        String divisibleBy_3 = "";
+        String divisibleBy_5 = "";
+
+
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0 && i % 15 == 0) {
+                divisibleBy_3_5_15 += i + " ";
+            } else if (i % 3 == 0 && !(i % 15 == 0)) {
+                divisibleBy_3 += i + " ";
+            } else if (i % 5 == 0 && !(i % 15 == 0)) {
+                divisibleBy_5 += i + " ";
+            }
+
+        }
+
+        System.out.print("Divisible By 15: " + divisibleBy_3_5_15 + "\n");
+        System.out.print("Divisible By 3: " + divisibleBy_3 + "\n");
+        System.out.print("Divisible By 5: " + divisibleBy_5 + "\n");
+
+
     }
 }
