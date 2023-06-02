c54a8adb8ad8effc8a95010c5fc35ff359eb1472
Joceline <jocelineelyaskhalil@gmail.com>
2023-06-02 09:04:09 -0400

    task 2

--- src/week_2/Joceline.java
+++ src/week_2/Joceline.java
@@ -18,5 +18,44 @@ public class Joceline {
 
         System.out.println("Value of x:" + x);
         System.out.println("Value of y:" + y);
+
+        System.out.println("------------------------------------------");
+
+        String divisibleBy3 = "";
+        String divisibleBy5 = "";
+        String divisibleBy15 ="";
+
+        for (int i=1; i<100; i++){
+            if(i%3==0){
+                divisibleBy3 += i+" ";
+                System.out.println("Divisible by 3: "+divisibleBy3 );
+
+            }
+        }
+
+
+        for (int i=1; i<100; i++){
+            if(i%5==0){
+                divisibleBy5 += i+" ";
+                System.out.println("Divisible by 5: "+divisibleBy5 );
+
+            }
+        }
+
+        for (int i=1; i<100; i++){
+            if(i%15==0){
+                divisibleBy15 += i+" ";
+                System.out.println("Divisible by 15: "+divisibleBy15 );
+
+            }
+        }
+
+
+
+
+
     }
+
+
+
 }
