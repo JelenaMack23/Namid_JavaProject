fea76f068537d033f03f9219d3a3ad43630d3fee
nkfl2022 <nkfl@yahoo.com>
2023-06-02 17:59:05 -0400

    created class for week3 tasks

---
+++ src/week_3/Natalia.java
@@ -0,0 +1,14 @@
+package week_3;
+
+public class Natalia {
+
+}
+/*
+1. Numbers - Prime numbers
+   Write a method that can check if a number is prime or not
+
+2. Numbers - Reverse negative number
+   Write a return method that can reverse negative number and return it as int
+
+   ex: num= -56     result = -65
+ */
