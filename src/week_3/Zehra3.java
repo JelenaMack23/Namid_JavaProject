c095648af62f9c30163441be9968902d9ad28885
Zehra <zehragullersener@gmail.com>
2023-06-07 16:36:02 -0400

    Zehra3 class updated

--- src/week_3/Zehra3.java
+++ src/week_3/Zehra3.java
@@ -12,12 +12,14 @@ Write a method that can check if a number is prime or not
         boolean isPrime =  isPrime(number);
         System.out.println(number + " is prime: " + isPrime);
 
+
+        int  a = -29;
+        System.out.println(rev(a));
     }
     public static boolean isPrime(int number) {
-        /*define a method named "isPrime" that takes integer parameter representing the number to be checked. This method will return a boolean value indicating whether the number is prime or not.
-         */
+        /*define a method named "isPrime" that takes integer parameter representing the number to be checked. This method will return a boolean value indicating whether the number is prime or not.*/
 
-
+/*
         if (number < 2) {// check the number is less than 2,as prime numbers are greater than or equal to 2.If the number is less than 2, return "false".since it can not be prime.
             return false;
         }
@@ -29,14 +31,63 @@ Write a method that can check if a number is prime or not
 
         }
         return true;
+      */
+
+        // Second solution of task 1
+        int count = 0;
+        boolean isPrime = true;
+
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                count = count + 1;
+            }
+        }
+        if (count <= 2) {
+            return isPrime;
+        }
+
+        return false;
+
     }
+/*
+
+2. Numbers -- Reverse negative number
+Write a return method that can reverse negative number and return it as int
+ */
+
+    public static int rev(int a) {
+        int b = a * (-1);
+        String c = "" + b;
+        //System.out.println(c);
+        int length = c.length();
+        //System.out.println(length);
+        int res = 0;
+        if (a >= 0) {
+            System.out.println(a + " is not a negative number");
+            System.exit(1);
+        } else {
+            String d = "";
+            for (int i = length - 1; i >= 0; i--) {
+
+                d = d + "" + c.charAt(i);
+                //System.out.println(d);
+
+            }
+            int e = Integer.parseInt(d);
+            int f = e * (-1);
+            //System.out.println(f);
+            res = f;
+        }
+        return res;
+
+    }
+
 
 
 }
 
 
 
-//Another solution for task1
 
 
 
