fa52b6be797d6f7f13bb5b2732ab815ca6a8d322
nkfl2022 <nkfl@yahoo.com>
2023-06-07 18:50:50 -0400

    week3 - task1 solution

--- src/week_3/Natalia.java
+++ src/week_3/Natalia.java
@@ -2,13 +2,55 @@ package week_3;
 
 public class Natalia {
 
+    public static void main(String[] args) {
+
+        primeNumber(3);
+        primeNumber(7);
+        primeNumber(56);
+        primeNumber(89);
+        primeNumber(2);
+        primeNumber(17);
+        primeNumber(82);
+        primeNumber(77);
+        primeNumber(35);
+        primeNumber(-5);
+    }
+
+
+
+
+    //1. Numbers - Prime numbers
+    //Write a method that can check if a number is prime or not
+    public static void primeNumber(int num){
+
+        int factor = 0;
+
+        if (num > 1) {
+
+            for (int i = 1; i <= num; i++) {
+                if(num%i==0){
+                    factor+=1;
+                }
+            }
+
+        }else {
+            System.err.println("Number cannot be negative.");
+            System.exit(1);
+        }
+        if (factor>2){
+            System.out.println(num + " is composite number");
+        }else {
+            System.out.println(num + " is prime number");
+        }
+    }
+
+
+    //2. Numbers - Reverse negative number
+    //Write a return method that can reverse negative number and return it as int
+
+    //ex: num= -56     result = -65
+
+
+
 }
-/*
-1. Numbers - Prime numbers
-   Write a method that can check if a number is prime or not
 
-2. Numbers - Reverse negative number
-   Write a return method that can reverse negative number and return it as int
-
-   ex: num= -56     result = -65
- */
