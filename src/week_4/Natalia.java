8e5b75102cc47dd13d6a145eb9c8a6d8eb205b43
nkfl2022 <nkfl@yahoo.com>
2023-06-16 11:32:09 -0400

    week4 task 1

--- src/week_4/Natalia.java
+++ src/week_4/Natalia.java
@@ -10,6 +10,33 @@ public class Natalia {
     //1. String - Frequency of Characters
     //Write a return method that can find the frequency of chars
     //ex: frequesncyOfChars("AAABBCDD") ==> A3B2C1D2
+    public static String frequencyOfChars(String stringOfChars){
+
+        String result = "";
+
+
+
+        for (int j = 0; j < stringOfChars.length(); j++) {
+
+            char ch = stringOfChars.charAt(j);
+            int count = 0;
+
+            for (int i = 0; i < stringOfChars.length(); i++) {
+
+                if (stringOfChars.charAt(i) == ch){
+                    count++;
+                }
+            }
+
+            if (result.contains("" + ch)){
+                continue;
+            }
+
+            result += ch + "" + count;
+        }
+
+        return result;
+    }
 
 
 
