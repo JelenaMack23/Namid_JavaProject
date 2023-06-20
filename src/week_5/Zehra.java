fe0deffcd0bdf2f9e6515826dff07e79c114ff51
Zehra <zehragullersener@gmail.com>
2023-06-19 21:29:44 -0400

    Zehra class created

--- src/week_5/Zehra.java
+++ src/week_5/Zehra.java
@@ -2,5 +2,15 @@ package week_5;
 
 public class Zehra {
 
-    // New tasks will come here
+    /*
+
+  Task1:  String -- Find the unique
+  Write a return method that can find the unique characters from the String Ex: unique("AAABBBCCCDEF") ==> "DEF";
+
+  Task2:
+  String -- Reverse
+  Write a return method that can reverse String Ex: Reverse("ABCD"); ==> DCBA
+
+     */
+
 }
