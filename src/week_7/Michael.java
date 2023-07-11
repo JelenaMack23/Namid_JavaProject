73d1ff141562fa609035e6fd10aaa37f2d1a504f
Ersin KAYHAN <dekked2438@gmail.com>
2023-07-11 13:17:16 +0300

    Week 7_Solutions

--- src/week_7/Michael.java
+++ src/week_7/Michael.java
@@ -1,5 +1,6 @@
 package week_7;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Michael {
@@ -9,6 +10,12 @@ public class Michael {
         System.out.println( "Min value_1"+  minValue_1(arr1));
 
         System.out.println("min value_2= "+minValue_2(arr1));
+
+       sortingArrayAsc(arr1);
+       sortingArrayDesc(arr1);
+
+
+
     }
 
     public static int minValue_1(int[] n) {
@@ -23,6 +30,48 @@ public class Michael {
     public static int minValue_2( int[]  n ) {
         Arrays.sort( n );
         return  n [ 0 ];
+    }
+
+    public static void sortingArrayAsc(int[] arr) {
+        ArrayList<Integer> list = new ArrayList();
+        for(int each: arr) {
+            list.add(each);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (list.get(i) < list.get(j)) {
+                    Integer temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+
+        System.out.println("Sorted array in ascending order: "+Arrays.toString(arr));
+        for(int i=0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+
+    }
+
+    public static void sortingArrayDesc(int[] arr) {
+        ArrayList<Integer> list = new ArrayList();
+        for(int each: arr) {
+            list.add(each);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (list.get(i) > list.get(j)) {
+                    Integer temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+        for(int i=0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        System.out.println("Sorted Array in Decending order: "+Arrays.toString(arr));
     }
 
 
