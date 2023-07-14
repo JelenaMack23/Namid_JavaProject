fee2871ac85e784e29c2f12962e20d072901171d
Nina_Cydeo <marko_lapcevic@yahoo.com>
2023-07-14 11:29:55 -0400

    Week 8, task 2

---
+++ src/Week_8/Nina.java
@@ -0,0 +1,25 @@
+package Week_8;
+
+import java.util.Arrays;
+import java.util.Arrays;
+
+public class Nina {
+
+    //Array-move zero to the end
+    public static int[] arrayMoveZerosToEnd(int[] arr){
+        int[] result=new int[arr.length-1];
+
+        for (int i = 0, j=0; i < arr.length; i++) {
+            if (arr[i]!=0){
+                result[j++]=arr[i];
+            }
+        }
+
+        return result;
+    }
+
+    }
+
+
+
+
